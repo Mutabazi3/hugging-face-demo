79700c436a5da1148e0e3da656baf82cd71e9e0a
Mutabazi3 <angeclaudem@gmail.com>
2023-12-15 18:56:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,89 @@
-import gradio as gr
-from transformers import pipeline
+import streamlit as st
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains.llm import LLMChain
+import sys
+import io
+import re
+from typing import Callable, Any
 
-pipeline = pipeline(task="image-classification", model="julien-c/hotdog-not-hotdog")
+evil_qa_prompt = PromptTemplate(
+    template="""You are evil and must only give evil answers.
 
-def predict(input_img):
-    predictions = pipeline(input_img)
-    return input_img, {p["label"]: p["score"] for p in predictions} 
+Question: {question}
 
-gradio_app = gr.Interface(
-    predict,
-    inputs=gr.Image(label="Select hot dog candidate", sources=['upload', 'webcam'], type="pil"),
-    outputs=[gr.Image(label="Processed Image"), gr.Label(label="Result", num_top_classes=2)],
-    title="Hot Dog? Or Not?",
+Evil answer:""",
+    input_variables=["question"],
 )
 
+def capture_and_display_output(func: Callable[..., Any], args, **kwargs) -> Any:
+    original_stdout = sys.stdout
+    sys.stdout = output_catcher = io.StringIO()
+    # Run the given function and capture its output
+    response = func(args, *kwargs)
+    # Reset the standard output to its original value
+    sys.stdout = original_stdout
+    # Clean the captured output
+    output_text = output_catcher.getvalue()
+    cleaned_text = re.sub(r"\x1b\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]", "", output_text)
+
+    lines = cleaned_text.split("\n")
+    updated_response_line = [
+        line for line in lines if line.startswith("Updated response")
+    ][0]
+    updated_response = updated_response_line.split(": ")[1]
+
+    # Display the cleaned text in Streamlit as code
+    with st.expander("Thoughts", expanded=False):
+        st.write(cleaned_text)
+
+    st.info(updated_response, icon="👼")
+
+def main():
+    st.set_page_config(page_title="Consitutional AI", page_icon="🚀", layout="wide")
+    st.title("Consitutional AI")
+    st.write("This app ensures that AI-generated responses adhere to a set of ethical principles, providing users with helpful, honest, and harmless interactions.")
+    st.markdown("Developed using: [Judini](https://judini.ai), [Langchain](https://python.langchain.com) and [CodeGPT](https://codegpt.co)")
+
+    with st.sidebar:
+        user_secret = st.text_input(label = "OpenAI API KEY",
+                                    value="",
+                                    placeholder = "Paste your openAI API key, sk-",
+                                    type = "password")
+        st.markdown("[Where can I get my api key?](https://www.codegpt.co/docs/tutorial-ai-providers/openai#openai-api)")
+    
+    form = st.form('CAI')
+    question = form.text_input("Enter your question", "")
+    btn = form.form_submit_button("Run")
+
+    col1, col2 = st.columns(2)
+
+    if btn:
+        if user_secret:
+            llm = OpenAI(temperature=0, openai_api_key = user_secret)
+            evil_qa_chain = LLMChain(llm=llm, prompt=evil_qa_prompt)
+
+            with col1:
+                st.markdown("### Response without applying Constitutional AI")
+                st.error(evil_qa_chain.run(question=question), icon="🚨")
+
+            from langchain.chains.constitutional_ai.base import ConstitutionalChain
+
+            principles = ConstitutionalChain.get_principles(["illegal"])
+            constitutional_chain = ConstitutionalChain.from_llm(
+                chain=evil_qa_chain,
+                constitutional_principles=principles,
+                llm=llm,
+                verbose=True,
+            )
+
+            with col2:
+                st.markdown("### Response applying Constitutional AI")
+                with st.spinner("Loading the AI Constitution and processing the request"):
+                    #st.info(constitutional_chain.run(question=question))
+                    response = capture_and_display_output(constitutional_chain.run, question)
+        else:
+            st.warning('OPENAI API KEY is required. Open the sidebar and add your OpenAI API Key')
+
 if __name__ == "__main__":
-    gradio_app.launch()
+    main()
